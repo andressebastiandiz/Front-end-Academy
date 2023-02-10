e62717c568c66fc949c5623f131f678026b154ff
andressebastiandiz <andres.diz@bue.edu.ar>
2023-02-10 12:25:04 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function perfil () {
+    document.getElementById("perfilA").innerHTML="Hola, mi nombre es"
+    document.getElementById("perfilB").innerHTML="Jimena Troglio"
+}
+
+function mail () {
+    document.getElementById("perfilA").innerHTML="Mi correo electrónico es"
+    document.getElementById("perfilB").innerHTML="j.troglio@example.com"
+}
+
+function fecha() {
+    document.getElementById("perfilA").innerHTML="Mi fecha de nacimiento es"
+    document.getElementById("perfilB").innerHTML="15/01/2001"
+}
+
+function lugar() {
+    document.getElementById("perfilA").innerHTML="Mi dirección es"
+    document.getElementById("perfilB").innerHTML="Avenida Rivadavia 701"
+}
+
+function tel() {
+    document.getElementById("perfilA").innerHTML="Mi teléfono es"
+    document.getElementById("perfilB").innerHTML="(574) 342-5022"
+}
